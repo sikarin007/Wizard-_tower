e859df3390022c7cfacaae68fd0b14aeac6f1496
siakrin <sikarin.pu.68@ubu.ac.th>
2026-03-12 00:35:14 +0700

    fix file 1

--- config.py
+++ config.py
@@ -12,3 +12,11 @@ player_jump_time_gravity = 0.01
 player_jump_time_height = 100
 player_jump_time_time = 0.5
 
+#tower position
+TOWER_POS = (screen_width // 2, screen_height // 2)
+
+#enemy
+ENEMY_SPEED = 2
+ENEMY_SIZE = 32
+ENEMY_COLOR = (255, 0, 0)
+ENEMY_POS = (screen_width // 2, screen_height // 2)
