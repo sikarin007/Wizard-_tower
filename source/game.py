0b245a2f27117713caa165bfcdc709f8defb0554
siakrin <sikarin.pu.68@ubu.ac.th>
2026-03-11 22:45:54 +0700

    first commit

---
+++ source/game.py
@@ -0,0 +1,19 @@
+import pygame
+
+import pygame
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1280, 720))
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+            self.screen.fill((0, 0, 0))
+            pygame.display.flip()
+            self.clock.tick(60)
+
